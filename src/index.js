616bb0d8be079bee43fbd9b677df8f5576d0e7ab
Joaquim Franco <joaquimlcbfranco@gmail.com>
2025-02-20 19:59:41 +0000

    Fetch buttons and call functions when clicked

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import "./styles.css";
 import { homepageLoad } from "./home.js";
+import { menuLoad } from "./menu.js";
 
-// homepageLoad();
+(function displayController() {
+    const contentDiv = document.querySelector('#content');
+    const navLinks = document.querySelectorAll('nav > button');
+    navLinks.forEach((button) => {
+        button.addEventListener('click', () => {
+            contentDiv.style.opacity = "0"; 
+
+            setTimeout(() => {
+                while (contentDiv.firstChild) {
+                    contentDiv.removeChild(contentDiv.firstChild);
+                }
+
+                if (button.id === 'home') homepageLoad();
+                if (button.id === 'menu') menuLoad();
+                if (button.id === 'about') aboutLoad();
+
+                contentDiv.style.opacity = '1';
+
+            }, 200);
+
+
+        })
+    })
+})();
+
+homepageLoad();
+
